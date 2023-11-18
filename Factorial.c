cb3bae402cf4aca1053441a5b033788535700027
Vladislav Atanassov <vlavalat@gmail.com>
2023-11-18 16:45:11 +0200

    Added // return number == 1 ? number : number * factorial(number - 1); as a comment

--- Factorial.c
+++ Factorial.c
@@ -8,6 +8,9 @@ int factorial(int number)
     }
 
     return number * factorial(number - 1);
+
+    // Also the if you want one line solution
+    // return number == 1 ? number : number * factorial(number - 1);
 }
 
 /*
